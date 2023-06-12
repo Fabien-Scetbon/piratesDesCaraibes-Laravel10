2b8905e8d909aa729a6bb3f4ae0097129379c80d
Fabien-Scetbon <fscetbon@gmail.com>
2023-06-12 16:15:25 +0200

    auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+
+class AuthController extends Controller
+{
+    public function index(): View
+    {
+        return view('auth.login');
+    }
+
+    public function registration(): View
+    {
+        return view('auth.registration');
+    }
+
+    public function postLogin(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');  // only : ne recupere que email et pass dans l'obj $request
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard')
+                ->withSuccess('You have Successfully loggedin');
+        }
+
+        return redirect("login")->withSuccess('Oppes! You have entered invalid credentials');
+    }
+}
