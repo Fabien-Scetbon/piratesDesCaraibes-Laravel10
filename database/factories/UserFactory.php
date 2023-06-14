73ed504ac114f56e6304c191bc4f65b2fc46f035
Fabien-Scetbon <fscetbon@gmail.com>
2023-06-14 18:27:46 +0200

    migrations-seeders

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'age'               => $this->faker->numberBetween($min = 16, $max = 60),
-            'description'       => $this->faker->text($maxNbChars = 20),
+            'description'       => $this->faker->text($maxNbChars = 50),
             'navire_id'         => Navire::all()->random()->id,
             'is_capitaine'      => random_int(0, 1),
             'remember_token'    => Str::random(10),
